9a3d3ffce15deb875088ce1ac211cca6de0d582d
Jacky Lau <jackylau0126@gmail.com>
2019-09-27 03:04:19 +0000

    Implemented TimesForMyLocation for original files and implemented promise method to the same assignment on a second file

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIp  } = require('./iss');
 const { fetchISSFlyOverTimes  } = require('./iss');
-
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
 fetchMyIP((error, ip) => {
   fetchCoordsByIp (ip, (error, coords) => {
     fetchISSFlyOverTimes (coords, (error, passes) => {
-      if (error) {
-        console.log("It didn't work!", error);
-        return;
-      }
-      console.log(passes);
+      nextISSTimesForMyLocation((error, passTimes) => {
+        if (error) {
+          return console.log("It didn't work!", error);
+        }
+        // success, print out the deets!
+        console.log(passTimes);
+      });
     });
   });
 });
--- iss.js
+++ iss.js
@@ -62,7 +62,24 @@ const fetchISSFlyOverTimes = function(coords, callback) {
   });
 };
 
+const nextISSTimesForMyLocation = function(callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      return callback(error, null);
+    }
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+      fetchISSFlyOverTimes(loc, (error, passes) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, passes);
+      });
+    });
+  });
+};
 
 
-
-module.exports = { fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes };
+module.exports = { fetchMyIP, fetchCoordsByIp, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
